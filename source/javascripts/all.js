9a605de8702cb288b5b1a968149d09eb7b733a9b
holleyy <alex.holley@artos.io>
2018-11-28 11:31:26 +0000

    testing and assumption

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,14 @@
 //= require ./all_nosearch
 //= require ./app/_search
+
+
+  // new window externals
+$(function() {
+  $(‘body’).delegate(“a”,”click”,function() {
+  var a = new RegExp(‘/’ + window.location.host + ‘/’);
+  if(!a.test(this.href)) {
+  event.preventDefault();
+  event.stopPropagation();
+  window.open(this.href, ‘_blank’);
+  }
+  });
