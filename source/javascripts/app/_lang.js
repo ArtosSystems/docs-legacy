4bc6994da9f3d319587b4aa2323de3d4ac6467da
holleyy <alex.holley@artos.io>
2018-11-28 11:24:27 +0000

    cmonnnn

--- source/javascripts/app/_lang.js
+++ source/javascripts/app/_lang.js
@@ -161,4 +161,17 @@ under the License.
       return false;
     });
   });
+
+
+  // new window externals
+$(function() {
+  $(‘body’).delegate(“a”,”click”,function() {
+  var a = new RegExp(‘/’ + window.location.host + ‘/’);
+  if(!a.test(this.href)) {
+  event.preventDefault();
+  event.stopPropagation();
+  window.open(this.href, ‘_blank’);
+  }
+  });
+});
 })();
