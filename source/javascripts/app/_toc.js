1535b9d8c8fa10464f6f8dfed4cda1378b146ab3
holleyy <alex.holley@artos.io>
2018-11-28 11:23:06 +0000

    remove quickly

--- source/javascripts/app/_toc.js
+++ source/javascripts/app/_toc.js
@@ -120,14 +120,3 @@
 
   window.loadToc = loadToc;
 })();
-
-// new window externals
-
-$(‘body’).delegate(“a”,”click”,function() {
-var a = new RegExp(‘/’ + window.location.host + ‘/’);
-if(!a.test(this.href)) {
-event.preventDefault();
-event.stopPropagation();
-window.open(this.href, ‘_blank’);
-}
-});
