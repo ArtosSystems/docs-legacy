837bfcccedcae1acf9b05249304a6020051ce741
holleyy <alex.holley@artos.io>
2018-11-28 11:30:06 +0000

    fix

--- source/javascripts/app/_lang.js
+++ source/javascripts/app/_lang.js
@@ -163,15 +163,5 @@ under the License.
   });
 
 
-  // new window externals
-$(function() {
-  $(‘body’).delegate(“a”,”click”,function() {
-  var a = new RegExp(‘/’ + window.location.host + ‘/’);
-  if(!a.test(this.href)) {
-  event.preventDefault();
-  event.stopPropagation();
-  window.open(this.href, ‘_blank’);
-  }
-  });
 });
 })();
