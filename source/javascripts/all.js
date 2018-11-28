dbf7169296540082710b3864b2a392cdca33d244
holleyy <alex.holley@artos.io>
2018-11-28 11:27:49 +0000

    lol

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,2 @@
 //= require ./all_nosearch
 //= require ./app/_search
-
-$(function() {
-  $(‘body’).delegate(“a”,”click”,function() {
-var a = new RegExp(‘/’ + window.location.host + ‘/’);
-if(!a.test(this.href)) {
-event.preventDefault();
-event.stopPropagation();
-window.open(this.href, ‘_blank’);
-}
-});
